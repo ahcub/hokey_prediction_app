315074b6e1f6903e38dff1671c294d46a1bbcac3
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2015-12-09 22:10:09 +0200

    !U add new data and new formula !R restructure project

--- sandbox/hockey_prediction.py
+++ sandbox/hockey_prediction.py
@@ -1,8 +1,12 @@
+import re
+
 from html_table_parser.parser import HTMLTableParser
 from lxml.html import fromstring, tostring
 from pandas import DataFrame
 from numpy import array
 import pandas as pd
+
+from data_utils import get_matches_info, get_players_data_from_matches_stats
 
 data = {
     'hits_count_mult_success_percent': {
@@ -35,34 +39,70 @@ data = {
     }
 }
 if __name__ == '__main__':
-    a = 'http://www.hokej.cz/tipsport-extraliga/player-stats/detailni?stats-menu-section=shots&stats-filter-season=2015&stats-filter-competition=5574&do=stats-view-pager-all'
+    a = 'http://www.hokej.cz/tipsport-extraliga/zapasy?matchList-view-displayAll=1&matchList-filter-season=2015&matchList-filter-competition=5574'
 
     import requests
 
     headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-               "Accept-Encoding": "gzip, deflate, sdch",
-               "Accept-Language": "en-US,en;q=0.8,uk;q=0.6,ru;q=0.4",
-               "Cache-Control": "max-age=0",
-               "Connection": "keep-alive",
-               "Cookie": "bblpasync=1449584613947; bblosync=1449585458940; __gfp_64b=sGN1O8XUlhJbt4ZWEMs2RKkIqL7nbIkleQ1NtqNQzx..M7; ibbid=BBID-01-01242986113897072; PHPSESSID=0i2j9hv3h877a3tullca1vfie4; nette-browser=gnqmgg5fbu; _ga=GA1.2.834154638.1449584608; _gat=1; __utmt=1; __utmt_trac2=1; __utma=47860542.834154638.1449584608.1449584610.1449584608.1; __utmb=47860542.4.10.1449584610; __utmc=47860542; __utmz=47860542.1449584610.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); popupShown=true; scrollingDivDisabled=true",
-               "Host": "www.hokej.cz",
-               "Upgrade-Insecure-Requests": "1",
-               "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36", }
+"Accept-Encoding": "gzip, deflate, sdch",
+"Accept-Language": "en-US,en;q=0.8,uk;q=0.6,ru;q=0.4",
+"Cache-Control": "max-age=0",
+"Connection": "keep-alive",
+"Cookie": "__gfp_64b=V7olMB1z1eHHz1PITzwE5Wc4548gu8NNKiDuhHfpYB3.J7; popupShown=true; scrollingDivDisabled=true; ibbid=BBID-01-01228999950496682; _gat=1; __utmt=1; __utmt_trac2=1; PHPSESSID=f84tqsoduqpjj3cc4gvjrrsjn0; nette-browser=anoda2xp6a; _ga=GA1.2.509669587.1449483158; __utma=47860542.509669587.1449483158.1449670439.1449676216.19; __utmb=47860542.12.10.1449676216; __utmc=47860542; __utmz=47860542.1449483158.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)",
+"Host": "www.hokej.cz",
+"Upgrade-Insecure-Requests": "1",
+"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36",}
 
-    # header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
+
+    df = get_players_data_from_matches_stats(a, headers)
+    print(df.to_dict())
+    # print(df)
+    # with open(r'D:\GitHub\hockey_prediction_app\sandbox\table_example.html', encoding='utf-8') as file_:
+    #     s = file_.read()
+    #     p = HTMLTableParser()
+    #     p.feed(s)
+    #     table_set = array([table[1:] for table_group in p.tables for table in table_group])
+    #     df2 = DataFrame(table_set[1:], columns=table_set[0])
+    #     df2 = df2.convert_objects(convert_numeric=True)
+    #     multiplication_result = df2.get('S/Z') * df2.get('RÚS')
+    #     df3 = DataFrame({'Name': df2.get('Jméno'), 'Team': df2.get('Tým'), 'Probability': multiplication_result})
+    #
+    # result_df = df.merge(df3, left_index=True, right_on='Name')
+    #
+    # print(result_df)
+    # print(result_df.get('Name'))
+    # print(result_df.get('Team'))
+
+
     # r = requests.get(a, headers=headers)
-
-    # v = get_url_response(a)
-    # print(v)
     #
-
-    # print(r.content)
+    # # print(r.content)
     # doc = fromstring(r.content)
-    # c = doc.find_class('table-stats')
+    # c = doc.find_class('col-soupisky-home') + doc.find_class('col-soupisky-visitor')
     #
-    # table_string = tostring(c[0])
-    # p = HTMLTableParser()
-    # p.feed(table_string.decode('utf-8'))
+    # for el in c:
+    #     for table in el.findall('table'):
+    #         table_string = tostring(table, encoding='utf-8')
+    #         p = HTMLTableParser()
+    #         decoded_str = table_string.decode('utf-8')
+    #         # print(decoded_str)
+    #         p.feed(decoded_str)
+    #         for row in p.tables[0][1:]:
+    #             if row[0]:
+    #                 print(extract_player_name(row[2], cut_last_element=False))
+    #         print('=' * 40)
+
+    # for el in c:
+    #     for sub_el in el.findall('ul/li'):
+    #         pattern = re.compile(r'(\d+\.\d+\.\d+)')
+    #         search_result = pattern.search(sub_el.text_content())
+    #         if search_result is not None:
+    #             print(search_result.group())
+    #
+    #
+    #     p = HTMLTableParser()
+    #     p.feed(table_string.decode('utf-8'))
+    #     print(p.tables)
     #
     # for table_set in p.tables:
     #     for table in table_set[1:]:
@@ -71,20 +111,20 @@ if __name__ == '__main__':
     #             a = '%s %f | %s %f' % (table[1], scoring_probability, table[1], scoring_probability)
     #             print(a)
 
-    with open('example.html', 'rb') as file_:
-        s = file_.read().decode('utf-8')
-        p = HTMLTableParser()
-        p.feed(s)
-        table_set = array([table[1:] for table_group in p.tables for table in table_group])
-
-        df = DataFrame(table_set[1:], columns=table_set[0])
-        df = df.convert_objects(convert_numeric=True)
-        # for team in df.get('Tým').unique():
-        #     print(team)
-        multiplication_result = df.get('S/Z') * df.get('RÚS')
-        result_df = DataFrame({'Name': df.get('Jméno'), 'Team': df.get('Tým'), 'Probability':multiplication_result})
-        for team, indexes in result_df.groupby('Team').groups.items():
-            print(result_df.loc[indexes, ['Name', 'Probability']].set_index('Name').to_dict()['Probability'])
+    # with open('example.html', encoding='utf-8') as file_:
+    #     s = file_.read()
+    #     p = HTMLTableParser()
+    #     p.feed(s)
+    #     table_set = array([table[1:] for table_group in p.tables for table in table_group])
+    #
+    #     df = DataFrame(table_set[1:], columns=table_set[0])
+    #     df = df.convert_objects(convert_numeric=True)
+    #     # for team in df.get('Tým').unique():
+    #     #     print(team)
+    #     multiplication_result = df.get('S/Z') * df.get('RÚS')
+    #     result_df = DataFrame({'Name': df.get('Jméno'), 'Team': df.get('Tým'), 'Probability':multiplication_result})
+    #     for team, indexes in result_df.groupby('Team').groups.items():
+    #         print(result_df.loc[indexes, ['Name', 'Probability']].set_index('Name').to_dict()['Probability'])
         # for table_set in p.tables:
         #     for table in table_set[1:]:
         #         scoring_probability = float(table[7]) * float(table[-1])
